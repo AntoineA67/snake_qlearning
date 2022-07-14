cd0e12470b1bfbc6ad8fb7229062c80caccb0927
Antoine Adamy <antoine.adamy@gmail.com>
2022-07-14 20:40:33 +0200

    model_fonctionnel

--- agent.py
+++ agent.py
@@ -16,7 +16,8 @@ import torch.nn.functional as F
 from time import sleep
 import pygame
 import torchvision.transforms as T
-
+from plot import plot
+import os
 
 # set up matplotlib
 is_ipython = 'inline' in matplotlib.get_backend()
@@ -34,18 +35,80 @@ Transition = namedtuple('Transition',
 
 N_ACTIONS = 3
 REPLAY_MEMORY_SIZE = 100000
-LEARNING_RATE = .001
+LEARNING_RATE = .003
+
+class Linear_QNet(nn.Module):
+	def __init__(self, input_size, hidden_size, output_size):
+		super().__init__()
+		self.linear1 = nn.Linear(input_size, hidden_size)
+		self.linear2 = nn.Linear(hidden_size, output_size)
+
+	def forward(self, x):
+		x = F.relu(self.linear1(x))
+		x = self.linear2(x)
+		return x
+
+	def save(self, file_name='model.pth'):
+		model_folder_path = './model'
+		if not os.path.exists(model_folder_path):
+			os.makedirs(model_folder_path)
+
+		file_name = os.path.join(model_folder_path, file_name)
+		torch.save(self.state_dict(), file_name)
+
+class QTrainer:
+	def __init__(self, model, lr, gamma):
+		self.lr = lr
+		self.gamma = gamma
+		self.model = model
+		self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
+		self.criterion = nn.MSELoss()
+
+	def train_step(self, state, action, reward, next_state, done):
+		state = torch.tensor(state, dtype=torch.float)
+		next_state = torch.tensor(next_state, dtype=torch.float)
+		action = torch.tensor(action, dtype=torch.long)
+		reward = torch.tensor(reward, dtype=torch.float)
+		# (n, x)
+
+		if len(state.shape) == 1:
+			# (1, x)
+			state = torch.unsqueeze(state, 0)
+			next_state = torch.unsqueeze(next_state, 0)
+			action = torch.unsqueeze(action, 0)
+			reward = torch.unsqueeze(reward, 0)
+			done = (done, )
+
+		# 1: predicted Q values with current state
+		pred = self.model(state)
+
+		target = pred.clone()
+		for idx in range(len(done)):
+			Q_new = reward[idx]
+			if not done[idx]:
+				Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
+
+			target[idx][torch.argmax(action[idx]).item()] = Q_new
+	
+		# 2: Q_new = r + y * max(next_predicted Q value) -> only do this if not done
+		# pred.clone()
+		# preds[argmax(action)] = Q_new
+		self.optimizer.zero_grad()
+		loss = self.criterion(target, pred)
+		loss.backward()
+
+		self.optimizer.step()
 
 class DQN(nn.Module):
 
 	def __init__(self, h, w, outputs):
 		super(DQN, self).__init__()
-		self.conv1 = nn.Conv1d(2, 1, kernel_size=5, stride=2)
-		self.bn1 = nn.BatchNorm2d(1)
-		# self.conv2 = nn.Conv1d(16, 1, kernel_size=5, stride=2)
-		# self.bn2 = nn.BatchNorm1d(1)
-		# self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
-		# self.bn3 = nn.BatchNorm2d(32)
+		self.conv1 = nn.Conv2d(1, 16, kernel_size=5, stride=2)
+		self.bn1 = nn.BatchNorm2d(16)
+		self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
+		self.bn2 = nn.BatchNorm2d(32)
+		self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
+		self.bn3 = nn.BatchNorm2d(32)
 
 		# Number of Linear input connections depends on output of conv2d layers
 		# and therefore the input image size, so compute it.
@@ -65,11 +128,11 @@ class DQN(nn.Module):
 		x = F.relu(self.bn3(self.conv3(x)))
 		return self.head(x.view(x.size(0), -1))
 
-BATCH_SIZE = 128
+BATCH_SIZE = 100
 GAMMA = 0.999
-EPS_START = 0.9
+EPS_START = 0.95
 EPS_END = 0.05
-EPS_DECAY = 200
+EPS_DECAY = 500
 TARGET_UPDATE = 10
 
 # Get screen size so that we can initialize layers correctly based on shape
@@ -77,8 +140,10 @@ TARGET_UPDATE = 10
 # which is the result of a clamped and down-scaled render buffer in get_screen()
 # init_screen = get_screen()
 # _, _, screen_height, screen_width = init_screen.shape
-HEIGHT = 30
-WIDTH = 48
+HEIGHT = 12
+WIDTH = 20
+# HEIGHT = 30
+# WIDTH = 48
 
 # Get number of actions from gym action space
 n_actions = 3
@@ -106,7 +171,7 @@ def select_action(state):
 			# t.max(1) will return largest column value of each row.
 			# second column on max result is index of where max element was
 			# found, so we pick action with the larger expected reward.
-			print(type(policy_net(state).max(1)[1].view(1, 1)))
+			# print(type(policy_net(state).max(1)[1].view(1, 1)))
 			return policy_net(state).max(1)[1].view(1, 1)
 	else:
 		return torch.tensor([[random.randrange(n_actions)]], device=device, dtype=torch.long)
@@ -179,12 +244,150 @@ def optimize_model():
 		param.grad.data.clamp_(-1, 1)
 	optimizer.step()
 
+class Agent:
+
+	def __init__(self):
+		self.n_games = 0
+		self.epsilon = 0 # randomness
+		self.gamma = 0.9 # discount rate
+		self.memory = deque(maxlen=REPLAY_MEMORY_SIZE) # popleft()
+		self.model = Linear_QNet(11, 256, 3)
+		self.trainer = QTrainer(self.model, lr=.001, gamma=self.gamma)
+
+
+	def get_state(self, game: SnakeGame):
+		# head = game.snake[0]
+		# point_l = Point(head.x - 20, head.y)
+		# point_r = Point(head.x + 20, head.y)
+		# point_u = Point(head.x, head.y - 20)
+		# point_d = Point(head.x, head.y + 20)
+		
+		# dir_l = game.direction == Direction.LEFT
+		# dir_r = game.direction == Direction.RIGHT
+		# dir_u = game.direction == Direction.UP
+		# dir_d = game.direction == Direction.DOWN
+
+		# state = [
+		# 	# Danger straight
+		# 	(dir_r and game.is_collision(point_r)) or 
+		# 	(dir_l and game.is_collision(point_l)) or 
+		# 	(dir_u and game.is_collision(point_u)) or 
+		# 	(dir_d and game.is_collision(point_d)),
+
+		# 	# Danger right
+		# 	(dir_u and game.is_collision(point_r)) or 
+		# 	(dir_d and game.is_collision(point_l)) or 
+		# 	(dir_l and game.is_collision(point_u)) or 
+		# 	(dir_r and game.is_collision(point_d)),
+
+		# 	# Danger left
+		# 	(dir_d and game.is_collision(point_r)) or 
+		# 	(dir_u and game.is_collision(point_l)) or 
+		# 	(dir_r and game.is_collision(point_u)) or 
+		# 	(dir_l and game.is_collision(point_d)),
+			
+		# 	# Move direction
+		# 	dir_l,
+		# 	dir_r,
+		# 	dir_u,
+		# 	dir_d,
+			
+		# 	# Food location 
+		# 	game.food.x < game.head.x,  # food left
+		# 	game.food.x > game.head.x,  # food right
+		# 	game.food.y < game.head.y,  # food up
+		# 	game.food.y > game.head.y  # food down
+		# 	]
+		print(game.get_env())
+		return game.get_env()
+		# return np.array(state, dtype=int)
+
+	def remember(self, state, action, reward, next_state, done):
+		self.memory.append((state, action, reward, next_state, done)) # popleft if MAX_MEMORY is reached
+
+	def train_long_memory(self):
+		if len(self.memory) > BATCH_SIZE:
+			mini_sample = random.sample(self.memory, BATCH_SIZE) # list of tuples
+		else:
+			mini_sample = self.memory
+
+		states, actions, rewards, next_states, dones = zip(*mini_sample)
+		self.trainer.train_step(states, actions, rewards, next_states, dones)
+		#for state, action, reward, nexrt_state, done in mini_sample:
+		#	self.trainer.train_step(state, action, reward, next_state, done)
+
+	def train_short_memory(self, state, action, reward, next_state, done):
+		self.trainer.train_step(state, action, reward, next_state, done)
+
+	def get_action(self, state):
+		# random moves: tradeoff exploration / exploitation
+		self.epsilon = 80 - self.n_games
+		final_move = [0,0,0]
+		if random.randint(0, 200) < self.epsilon:
+			move = random.randint(0, 2)
+			final_move[move] = 1
+		else:
+			state0 = torch.tensor(state, dtype=torch.float)
+			prediction = self.model(state0)
+			move = torch.argmax(prediction).item()
+			final_move[move] = 1
+
+		return final_move
+
+
+def train():
+	plot_scores = []
+	plot_mean_scores = []
+	total_score = 0
+	record = 0
+	pygame.init()
+	font = pygame.font.SysFont('arial', 25)
+	agent = Agent()
+	game = SnakeGame(w=WIDTH, h=HEIGHT, font=font)
+	while True:
+		# get old state
+		state_old = agent.get_state(game)
+
+		# get move
+		final_move = agent.get_action(state_old)
+		# print(final_move)
+
+		# perform move and get new state
+		score, reward, done = game.play_step(final_move)
+		print(reward)
+		state_new = agent.get_state(game)
+
+		# train short memory
+		agent.train_short_memory(state_old, final_move, reward, state_new, done)
+
+		# remember
+		agent.remember(state_old, final_move, reward, state_new, done)
+
+		if done:
+			# train long memory, plot result
+			game.reset()
+			agent.n_games += 1
+			agent.train_long_memory()
+
+			if score > record:
+				record = score
+				agent.model.save()
+
+			print('Game', agent.n_games, 'Score', score, 'Record:', record)
+
+			plot_scores.append(score)
+			total_score += score
+			mean_score = total_score / agent.n_games
+			plot_mean_scores.append(mean_score)
+			plot(plot_scores, plot_mean_scores)
+	pygame.quit()
+
 def main():
 	
 	if SnakeGame.NORMAL_GAME:
 		pygame.init()
 		font = pygame.font.SysFont('arial', 25)
-		game = SnakeGame(font=font)
+		game = SnakeGame(w=WIDTH, h=HEIGHT, font=font)
 		sleep(2)
 		game_over = False
 		while not game_over:
@@ -213,28 +416,33 @@ def main():
 		# 	score = state[0]
 		# 	sleep(.4)
 
-		num_episodes = 50
+		num_episodes = 200
 		for i_episode in range(num_episodes):
 			# Initialize the environment and state
 			env.reset()
 			last_screen = env.get_env()
 			current_screen = env.get_env()
-			state = current_screen - last_screen
+			state = current_screen
+			# state = current_screen - last_screen
+			score = 0
 			for t in count():
+				# env.get_state(verbose=True)
 				# Select and perform an action
 				action = select_action(state)
 				action_game = np.zeros(3)
 				action_game[action.item()] = 1
-				print("Action:", action, action_game)
-				_, reward, done = env.play_step(action=action_game)
-				print(reward)
+				# print("Action:", action, action_game)
+				score, reward, done = env.play_step(action=action_game)
+				# if t % 10 == 0:
+				print(reward, score)
 				reward = torch.tensor([reward], device=device)
 
 				# Observe new state
 				last_screen = current_screen
 				current_screen = env.get_env()
 				if not done:
-					next_state = current_screen - last_screen
+					next_state = current_screen
+					# next_state = current_screen - last_screen
 				else:
 					next_state = None
 
@@ -247,10 +455,12 @@ def main():
 				# Perform one step of the optimization (on the policy network)
 				optimize_model()
 				if done:
-					episode_durations.append(t + 1)
-					plot_durations()
+					env.reset()
+					episode_durations.append(score)
+					# plot_durations()
 					break
 			# Update the target network, copying all weights and biases in DQN
+			print(score)
 			if i_episode % TARGET_UPDATE == 0:
 				target_net.load_state_dict(policy_net.state_dict())
 
@@ -266,4 +476,4 @@ def main():
 
 
 if __name__ == '__main__':
-	main()
+	train()
--- game.py
+++ game.py
@@ -21,8 +21,8 @@ BLUE1 = (0, 0, 255)
 BLUE2 = (0, 100, 255)
 BLACK = (0,0,0)
 
-BLOCK_SIZE = 30
-SPEED = 10
+BLOCK_SIZE = 20
+
 
 class SnakeGame:
 	
@@ -30,6 +30,7 @@ class SnakeGame:
 	NORMAL_GAME = False
 
 	def __init__(self, font, w=48, h=30):
+		self.speed = 1000
 		self.font = font
 		self.w = w * BLOCK_SIZE
 		self.h = h * BLOCK_SIZE
@@ -50,14 +51,20 @@ class SnakeGame:
 			self.direction = RIGHT
 			self.head = Point(self.w_blocks // 2, self.h_blocks // 2)
 		else:
-			self.direction = random.randint(0, 3)
-			self.head = Point(random.randint(1, self.w_blocks - 3), random.randint(1, self.h_blocks - 3))
+			self.direction = RIGHT
+			self.head = Point(self.w_blocks // 2, self.h_blocks // 2)
+			# self.direction = random.randint(0, 3)
+			# self.head = Point(random.randint(1, self.w_blocks - 3), random.randint(1, self.h_blocks - 3))
 
 		self.snake = [self.head,
 					  Point(self.head.x + (self.direction == LEFT) - (self.direction == RIGHT), self.head.y + (self.direction == UP) - (self.direction == DOWN)),
 					  Point(self.head.x + (self.direction == LEFT) * 2 - (self.direction == RIGHT) * 2, self.head.y + (self.direction == UP) * 2 - (self.direction == DOWN) * 2)]
-		print(self.direction, self.snake)
-		self.board = np.zeros((self.h_blocks, self.w_blocks), dtype=int)
+		# print(self.direction, self.snake)
+		# self.board = np.zeros((self.h_blocks, self.w_blocks), dtype=torch.cuda.)
+		self.n_steps = 0
+		self.board = torch.zeros((self.h_blocks, self.w_blocks))
+		self.explored = torch.zeros((self.h_blocks, self.w_blocks))
+		self.food_board = torch.zeros((self.h_blocks, self.w_blocks))
 
 		# self.board = np.zeros((self.h_blocks + 2, self.w_blocks + 2), dtype=int)
 		# self.board[0] = np.ones(self.board.shape[1])
@@ -66,26 +73,48 @@ class SnakeGame:
 		# self.board[:, -1] = np.ones(self.board.shape[0])
 
 		self.score = 0
-		self.food = None
-		self._place_food()
+		self.init_food()
+		# self._place_food()
 		self.frame = 0
 		self.game_over = False
 		self.reward = 0
 
-		for p in self.snake:
-			self.board[p.y, p.x] = 1
-		self.board[self.food.y, self.food.x] = 100
+		# for p in self.snake:
+		# 	self.board[p.y, p.x] = 100
+		# 	self.explored[p.y, p.x] = 1
+		self.board[self.head.y, self.head.x] = 50
+
+		for f in self.food:
+			self.food_board[f.y, f.x] = 255
+			self.board[f.y, f.x] = 255
+			
+		# for f in self.food:
+		# 	self.board[f.y, f.x] = 100
 		
+	def init_food(self):
+		self.food = {}
+		for i in range(1):
+			x = random.randint(0, self.w_blocks - 1)
+			y = random.randint(0, self.h_blocks - 1)
+			f = Point(x, y)
+			if self.food.get(f, 0) == 0:
+				self.food[f] = 1
+			else:
+				i -= 1
+	
 	def _place_food(self):
 		x = random.randint(0, self.w_blocks - 1)
 		y = random.randint(0, self.h_blocks - 1)
-		self.food = Point(x, y)
-		if self.food in self.snake:
-			self._place_food()
+		f = Point(x, y)
+		if self.food.get(f, 0) == 0 and f not in self.snake:
+			self.food[f] = 1
+			self.food_board[y, x] = 255
+			self.board[y, x] = 255
 		else:
-			self.board[y, x] = 100
+			self._place_food()
 		
 	def play_step(self, action=None):
+		self.n_steps += 1
 		if SnakeGame.NORMAL_GAME:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
@@ -108,24 +137,47 @@ class SnakeGame:
 
 		self._move(self.direction)
 		self.snake.insert(0, self.head)
-		self.board[self.snake[0].y, self.snake[0].x] = 1
-
+		f = list(self.food)[0]
+		# print(f)
+		self.reward = 10 / (abs(self.head.x - f.x) + abs(self.head.y - f.y) + 1)
+		# self.reward = -1 * self.n_steps + 100 / (abs(self.head.x - f.x) + abs(self.head.y - f.y) + 1)
+		# self.reward = 10 / (abs(self.head.x - f.x) + abs(self.head.y - f.y) + 1) - 10 * self.n_steps
+		# self.reward = 10 / (abs(self.head.x - f.x) + abs(self.head.y - f.y) + 1) + int(self.explored.sum())
+		# self.reward = int(self.explored.sum())
+		# self.reward = -self.n_steps // 100 + 1 / (abs(self.head.x - f.x) + abs(self.head.y - f.y) + 1)
+		if self.n_steps > 200:
+			self.game_over = True
 		if self._is_collision():
 			self.game_over = True
-			self.reward = -10
+			self.reward = - 10
 			return self.score, self.reward, self.game_over
 
-		if self.head == self.food:
+		for p in self.snake:
+			self.board[p.y, p.x] = 50
+		self.board[self.snake[0].y, self.snake[0].x] = 100
+		if self.explored[self.snake[0].y, self.snake[0].x]:
+			pass
+			# self.reward = -2
+		else:
+			self.explored[self.snake[0].y, self.snake[0].x] = 1
+		if self.food.get(self.head):
+			# self.speed = 5
+			self.n_steps = 0
+			self.food.pop(self.head)
 			self.score += 1
-			self.reward = 10
+			self.reward = 100
 			self._place_food()
 		else:
 			self.board[self.snake[-1].y, self.snake[-1].x] = 0
+			self.food_board[self.snake[-1].y, self.snake[-1].x] = 0
 			self.snake.pop()
 
 		if SnakeGame.DISPLAY:
 			self._update_ui()
-			self.clock.tick(SPEED)
+			self.clock.tick(self.speed)
+		
+		# if self.n_steps > 100:
+			# self.game_over = True
 
 		return self.score, self.reward, self.game_over
 	
@@ -139,7 +191,8 @@ class SnakeGame:
 			d = i / len(self.snake) * 6
 			pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x * BLOCK_SIZE + d, pt.y * BLOCK_SIZE + d, BLOCK_SIZE - d * 2, BLOCK_SIZE - d * 2))
 			pygame.draw.rect(self.display, BLUE2, pygame.Rect(pt.x * BLOCK_SIZE + 4 + d, pt.y * BLOCK_SIZE + 4 + d, 12 - d * 2, 12 - d * 2))
-		pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x * BLOCK_SIZE, self.food.y * BLOCK_SIZE, BLOCK_SIZE - 2, BLOCK_SIZE - 2))
+		for f in self.food:
+			pygame.draw.rect(self.display, RED, pygame.Rect(f.x * BLOCK_SIZE, f.y * BLOCK_SIZE, BLOCK_SIZE - 2, BLOCK_SIZE - 2))
 		text = self.font.render("Score: " + str(self.score), True, WHITE)
 		self.display.blit(text, [0, 0])
 		pygame.display.flip()
@@ -148,17 +201,35 @@ class SnakeGame:
 		# l2
 		# food_dist = np.sqrt(np.power(self.head.x - self.food.x, 2) + np.power(self.head.y - self.food.y, 2))
 		# l1
-		food_dist = abs(self.head.x - self.food.x) + abs(self.head.y - self.food.y)
+		# food_dist = abs(self.head.x - self.food.x) + abs(self.head.y - self.food.y)
 
 		if verbose:
-			print(food_dist)
-			print('\n'.join([' '.join([str(x) for x in self.board[i]]) for i in range(len(self.board))]))
+			# print(food_dist)
+			board = self.explored.numpy()
+			# board = self.board.numpy()
+			print('\n'.join([' '.join([str(x) for x in board[i]]) for i in range(len(board))]))
 			print('\n\n')
 
-		return (self.score, self.reward, self.game_over, food_dist, self.board)
+		# return (self.score, self.reward, self.game_over, food_dist, self.board)
 	
 	def get_env(self):
-		return torch.from_numpy(self.board)
+		# self.food_board[self.head.y, self.head.x] = 100
+		# return self.food_board.unsqueeze(0).unsqueeze(0)
+		# print(self.board.unsqueeze(0).unsqueeze(0))
+		f = list(self.food)[0]
+
+		s = Point(self.head.x + (self.direction == RIGHT) - (self.direction == LEFT), self.head.y - (self.direction == UP) + (self.direction == DOWN))
+		l = Point(self.head.x + (self.direction == UP) - (self.direction == DOWN), self.head.y - (self.direction == LEFT) + (self.direction == RIGHT))
+		r = Point(self.head.x + (self.direction == DOWN) - (self.direction == UP), self.head.y - (self.direction == RIGHT) + (self.direction == LEFT))
+		ds = int(s.x >= self.w_blocks or s.x < 0 or s.y >= self.h_blocks or s.y < 0 or s in self.snake)
+		dl = int(l.x >= self.w_blocks or l.x < 0 or l.y >= self.h_blocks or l.y < 0 or l in self.snake)
+		dr = int(r.x >= self.w_blocks or r.x < 0 or r.y >= self.h_blocks or r.y < 0 or r in self.snake)
+
+		return (int(self.direction == RIGHT),int(self.direction == DOWN),int(self.direction == LEFT),int(self.direction == UP), ds, dl, dr, int(f.x > self.head.x), int(f.x < self.head.x), int(f.y > self.head.y), int(f.y < self.head.y))
+
+		return (self.direction, self.score, self.head.x, self.head.y, 10 / (abs(self.head.x - f.x) + abs(self.head.y - f.y) + 1), f.x, f.y)
+		return self.board.flatten()
+		return self.board.unsqueeze(0).unsqueeze(0)
 		
 	def _move(self, direction):
 		self.head = Point(self.head.x + (direction == RIGHT) - (direction == LEFT),
